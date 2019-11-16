785466431dc0dd3fdc3bbc952520a4af9f291c0b
bobbjedi <bobbraunjedi@gmail.com>
2019-11-16 15:15:20 +0300

    configreader

--- server/helpers/configReader.js
+++ server/helpers/configReader.js
@@ -1,5 +1,12 @@
 const fs = require('fs');
-const configJson = fs.readFileSync(__dirname + '../../../config.js')
+
+const sep = __dirname.includes('/') ? '/' : '\\';
+const dirs = __dirname.split(sep);
+dirs.pop();
+dirs.pop();
+const dirName = dirs.join(sep);
+
+const configJson = fs.readFileSync(dirName + '/config.js')
     .toString()
     .replace('export default', '')
     .replace(';', '');
