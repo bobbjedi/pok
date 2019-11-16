d0336190f3c6e71dc7a2adaa3e7fb89f2b32ee97
bobbjedi <bobbraunjedi@gmail.com>
2019-11-16 15:00:47 +0300

    r5efach

--- app/controllers/table.js
+++ app/controllers/table.js
@@ -28,6 +28,7 @@ app.controller('TableController', ['$scope', '$rootScope', '$http', '$routeParam
             $scope.table = data.table;
             $scope.buyInAmount = data.table.maxBuyIn;
             $scope.betAmount = data.table.bigBlind;
+            console.log(data);
         });
 
         // Joining the socket room
