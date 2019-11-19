901c27507997c8ff3bbc5ba4ea6d4c38d4a7989d
RomanS <slep48@gmail.com>
2019-11-20 00:09:19 +0300

    -

--- app/init.js
+++ app/init.js
@@ -79,6 +79,7 @@ window.copyAddress = () => {
 
         if (document.fullscreenEnabled) {
             isFullScreen = true;
+            localStorage.setItem('isFullScreen', isFullScreen);
             requestFullscreen(document.documentElement);
         }
     };
@@ -87,6 +88,7 @@ window.copyAddress = () => {
 
     window.cancelFullscreenCustom = () => {
         isFullScreen = false;
+        localStorage.setItem('isFullScreen', isFullScreen);
         if (document.cancelFullScreen) {
             document.cancelFullScreen();
         } else if (document.mozCancelFullScreen) {
@@ -100,6 +102,10 @@ window.copyAddress = () => {
         if (isFullScreen) {
             return window.cancelFullscreenCustom();
         }
+        window.launchFullScreen();
+    };
+
+    if (localStorage.getItem('isFullScreen')){
         window.launchFullScreen();
     };
 })();
