ead9aed5274f274fd7aadfbfbb298d8846adf98f
jedy <bobbjedy@gmail.com>
2020-01-03 14:24:19 +0300

    aa

--- server/index.js
+++ server/index.js
@@ -777,9 +777,9 @@ $u.init({players, tables, eventEmitter});
 
 
 setTimeout(async () => {
-    Store.createMtt({tableSeatsCount: 6});
-    Store.system.mtt.users = ['Dev', 'Devid', 'DevZ', 'DevX', 'DevI', 'DevL', 'DevA', 'Dev1', 'DevY'];
-    Store.system.mtt.chips = 50;
-    Store.system.mtt.timeOutShufflePlayers = 1;
-    Store.startMtt();
+    // Store.createMtt({tableSeatsCount: 6});
+    // Store.system.mtt.users = ['Dev', 'Devid', 'DevZ', 'DevX', 'DevI', 'DevL', 'DevA', 'Dev1', 'DevY'];
+    // Store.system.mtt.chips = 50;
+    // Store.system.mtt.timeOutShufflePlayers = 1;
+    // Store.startMtt();
 }, 10000);
