705f22bf384e7d18c03cf3253ed47c066b9dc69f
bobbjedi <bobbraunjedi@gmail.com>
2020-02-15 21:20:58 +0300

    fix data

--- app/controllers/mixins/tourn.js
+++ app/controllers/mixins/tourn.js
@@ -22,6 +22,7 @@ export default ($scope, $rootScope) => {
     $scope.$watch('table.data.isMtt', isMtt => {
         if (isMtt) {
             socket.on('public-mtt', data => {
+                console.log(data, data.playersList)
                 if (data.playersList && data.playersList.length){
                     $scope.publicMtt = data;
                     concatPlayers();
