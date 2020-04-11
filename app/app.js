1de7e1b5161145c8465512c0cd6dd9ecca3eddaa
jedy <bobbjedy@gmail.com>
2020-04-11 22:02:34 +0300

    add err

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ import _ from 'underscore';
 import $u from './libs/utils';
 import socket from './services/socket.io';
 import voiceChat from './services/voiceChat';
-import push from './services/firebase';
+// import push from './services/firebase';
 
 const app = angular.module('app', ['ngRoute']).config(function($routeProvider, $locationProvider) {
     $routeProvider.when('/table-10/:tableId', {
@@ -126,7 +126,9 @@ app.run(function($rootScope) {
             $rootScope.$digest();
         } catch (e) { }
     };
-    push($rootScope);// оборачиваем пушами
+    
+    // push($rootScope);// оборачиваем пушами
+
     $rootScope.$watch('settings.coinName', coinName => socket.emit('changeCoinName', coinName)); // меняем депозит
     $rootScope.updateUser(true);
     $rootScope.depositCps = amount => {
